407e4059c4ce035510f5e89a1fb1fc19d75fc995
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-20 17:43:27 +0300

    IGenericRepository interface added

---
+++ NexusTix.Persistence/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using NexusTix.Domain.Entities.Common;
+using System.Linq.Expressions;
+
+namespace NexusTix.Persistence
+{
+    public interface IGenericRepository<T, TId> where T : BaseEntity<TId>
+    {
+        Task<bool> AnyAsync(TId id);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllPagedAsync(int pageNumber, int pageSize);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        Task<T?> GetByIdAsync(TId id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<bool> PassiveAsync(TId id);
+    }
+}
