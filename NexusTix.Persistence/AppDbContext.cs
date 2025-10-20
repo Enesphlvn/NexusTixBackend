cef8cd8887ef4de50b5d4cc1734addbb08f8914a
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-20 17:18:36 +0300

    AppDbContext class added.

---
+++ NexusTix.Persistence/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence
+{
+    public class AppDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<City>().HasQueryFilter(e => e.IsActive);
+            builder.Entity<District>().HasQueryFilter(e => e.IsActive);
+            builder.Entity<EventType>().HasQueryFilter(e => e.IsActive);
+            builder.Entity<Venue>().HasQueryFilter(e => e.IsActive);
+            builder.Entity<Event>().HasQueryFilter(e => e.IsActive);
+            builder.Entity<Ticket>().HasQueryFilter(e => e.IsActive);
+        }
+    }
+}
