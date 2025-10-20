6754be8ad28f33a6a09d64a69927d9f3bdc06814
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-20 16:15:19 +0300

    BaseEntity class Added

---
+++ NexusTix.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusTix.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
