614db200ff682c9a7e0dbaba6c7b19ce5153ea74
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-21 17:16:13 +0300

    CityRepository class added

--- NexusTix.Persistence/Repositories/Cities/CityRepository.cs
+++ NexusTix.Persistence/Repositories/Cities/CityRepository.cs
@@ -1,6 +1,46 @@
-﻿namespace NexusTix.Persistence.Repositories.Cities
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.Cities
 {
-    public class CityRepository
+    public class CityRepository : GenericRepository<City, int>, ICityRepository
     {
+        private readonly AppDbContext _context;
+
+        public CityRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesWithDistrictsAndVenuesAsync()
+        {
+            return await _context.Cities.Include(x => x.Districts).ThenInclude(x => x.Venues).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesWithDistrictsAsync()
+        {
+            return await _context.Cities.Include(x => x.Districts).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesWithVenuesAsync()
+        {
+            return await _context.Cities.Include(x => x.Venues).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<City?> GetCityWithDistrictsAndVenuesAsync(int id)
+        {
+            return await _context.Cities.Include(x => x.Districts).ThenInclude(x => x.Venues).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<City?> GetCityWithDistrictsAsync(int id)
+        {
+            return await _context.Cities.Include(x => x.Districts).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<City?> GetCityWithVenuesAsync(int id)
+        {
+            return await _context.Cities.Include(x => x.Venues).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
