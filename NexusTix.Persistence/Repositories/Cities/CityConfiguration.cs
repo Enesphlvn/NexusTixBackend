19f827ecf9ea7db1f050897c298dc89aff0165b7
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-21 17:28:22 +0300

    CityConfiguration class added

---
+++ NexusTix.Persistence/Repositories/Cities/CityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Cities
+{
+    public class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(x => x.Districts)
+                .WithOne(d => d.City)
+                .HasForeignKey(d => d.CityId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
