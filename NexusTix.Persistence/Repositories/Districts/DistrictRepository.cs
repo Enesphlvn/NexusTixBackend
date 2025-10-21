a669cfe2e9fb77f16a8a24a1a8df25d6518b5020
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-21 17:46:01 +0300

    IDsitrictRepository and DistrictRepository added

---
+++ NexusTix.Persistence/Repositories/Districts/DistrictRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusTix.Persistence.Repositories.Districts
+{
+    public class DistrictRepository : GenericRepository<District, int>, IDistrictRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DistrictRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<District>> GetDistrictsByCityAsync(int cityId)
+        {
+            return await _context.Districts.Where(x => x.CityId == cityId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<District>> GetDistrictsWithVenuesAsync()
+        {
+            return await _context.Districts.Include(x => x.Venues).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<District?> GetDistrictWithVenuesAsync(int id)
+        {
+            return await _context.Districts.Include(x => x.Venues).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
