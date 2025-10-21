95bf14e5eaa3f5b7366bbbba288944bb273c158e
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-21 16:57:30 +0300

    ICityRepository added

--- NexusTix.Persistence/Repositories/Cities/ICityRepository.cs
+++ NexusTix.Persistence/Repositories/Cities/ICityRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace NexusTix.Persistence.Repositories.Cities
-{
-    public interface ICityRepository
-    {
+﻿using NexusTix.Domain.Entities;
 
+namespace NexusTix.Persistence.Repositories.Cities
+{
+    public interface ICityRepository : IGenericRepository<City, int>
+    {
+        Task<City?> GetCityWithDistrictsAsync(int id);
+        Task<IEnumerable<City>> GetCitiesWithDistrictsAsync();
+        Task<City?> GetCityWithVenuesAsync(int id);
+        Task<IEnumerable<City>> GetCitiesWithVenuesAsync();
+        Task<City?> GetCityWithDistrictsAndVenuesAsync(int id);
+        Task<IEnumerable<City>> GetCitiesWithDistrictsAndVenuesAsync();
     }
 }
