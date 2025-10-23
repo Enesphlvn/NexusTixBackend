ab3c1e7523b1960c930d9d7d7127018c04954eb4
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 16:05:40 +0300

    EventTypeConfiguration added

---
+++ NexusTix.Persistence/Repositories/EventTypes/EventTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.EventTypes
+{
+    public class EventTypeConfiguration : IEntityTypeConfiguration<EventType>
+    {
+        public void Configure(EntityTypeBuilder<EventType> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.HasMany(et => et.Events)
+                .WithOne(e => e.EventType)
+                .HasForeignKey(e => e.EventTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
