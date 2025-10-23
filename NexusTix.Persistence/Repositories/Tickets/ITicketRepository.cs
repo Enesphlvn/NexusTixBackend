fc6e26a4469ac4e1a10363a5d55265398534d047
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 16:34:47 +0300

    ITicketRepoistory and TicketRepository added

---
+++ NexusTix.Persistence/Repositories/Tickets/ITicketRepository.cs
@@ -0,0 +1,13 @@
+﻿using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Tickets
+{
+    public interface ITicketRepository : IGenericRepository<Ticket, int>
+    {
+        Task<IEnumerable<Ticket>> GetTicketsByEventAsync(int eventId);
+        Task<Ticket?> GetTicketWithDetailAsync(int id);
+        Task<IEnumerable<Ticket>> GetTicketsWithDetailAsync();
+        Task<int> GetTicketCountByEventAsync(int eventId);
+        Task<IEnumerable<Ticket>> GetTicketsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate);
+    }
+}
---
+++ NexusTix.Persistence/Repositories/Tickets/TicketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.Tickets
+{
+    public class TicketRepository : GenericRepository<Ticket, int>, ITicketRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TicketRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GetTicketCountByEventAsync(int eventId)
+        {
+            return await _context.Tickets.CountAsync(x => x.EventId == eventId);
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTicketsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            return await _context.Tickets.Include(x => x.Event).Include(x => x.User)
+                .Where(x => x.PurchaseDate >= startDate && x.PurchaseDate <= endDate).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTicketsByEventAsync(int eventId)
+        {
+            return await _context.Tickets.Include(x => x.User).Where(x => x.EventId == eventId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTicketsWithDetailAsync()
+        {
+            return await _context.Tickets.Include(x => x.Event).Include(x => x.User).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Ticket?> GetTicketWithDetailAsync(int id)
+        {
+            return await _context.Tickets.Include(x => x.Event).Include(x => x.User).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
