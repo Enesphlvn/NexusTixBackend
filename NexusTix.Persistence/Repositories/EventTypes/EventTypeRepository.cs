b00fb46505588f42efd9ab5060c763bcdc600e0b
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 16:00:33 +0300

    IEventTypeRepository and EventTypeRepository added

---
+++ NexusTix.Persistence/Repositories/EventTypes/EventTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.EventTypes
+{
+    public class EventTypeRepository : GenericRepository<EventType, int>, IEventTypeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EventTypeRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<EventType>> GetEventTypesWithEventsAsync()
+        {
+            return await _context.EventTypes.Include(x => x.Events).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<EventType?> GetEventTypeWithEventsAsync(int id)
+        {
+            return await _context.EventTypes.Include(x => x.Events).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ NexusTix.Persistence/Repositories/EventTypes/IEventTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.EventTypes
+{
+    public interface IEventTypeRepository : IGenericRepository<EventType, int>
+    {
+        Task<EventType?> GetEventTypeWithEventsAsync(int id);
+        Task<IEnumerable<EventType>> GetEventTypesWithEventsAsync();
+    }
+}
