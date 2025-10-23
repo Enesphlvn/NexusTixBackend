4fa9957f8badaf88a6902a1503e3bf623159c0b4
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 17:10:45 +0300

    VenueConfiguration added

---
+++ NexusTix.Persistence/Repositories/Venues/VenueConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Venues
+{
+    public class VenueConfiguration : IEntityTypeConfiguration<Venue>
+    {
+        public void Configure(EntityTypeBuilder<Venue> builder)
+        {
+            builder.HasKey(v => v.Id);
+            builder.Property(v => v.Name).IsRequired().HasMaxLength(150);
+            builder.Property(v => v.Capacity).IsRequired();
+
+            builder.HasOne(v => v.District)
+                .WithMany(d => d.Venues)
+                .HasForeignKey(v => v.DistrictId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(v => v.Events)
+                .WithOne(e => e.Venue)
+                .HasForeignKey(e => e.VenueId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
