17e4034bc242839bf07c1340543aaa61da9051ee
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 16:42:02 +0300

    TicketConfiguration added

--- NexusTix.Persistence/Repositories/EventTypes/EventTypeConfiguration.cs
+++ NexusTix.Persistence/Repositories/EventTypes/EventTypeConfiguration.cs
@@ -8,8 +8,8 @@ namespace NexusTix.Persistence.Repositories.EventTypes
     {
         public void Configure(EntityTypeBuilder<EventType> builder)
         {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.HasKey(et => et.Id);
+            builder.Property(et => et.Name).IsRequired().HasMaxLength(50);
 
             builder.HasMany(et => et.Events)
                 .WithOne(e => e.EventType)
---
+++ NexusTix.Persistence/Repositories/Tickets/TicketConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Tickets
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.HasIndex(t => t.QRCodeGuid).IsUnique();
+            builder.Property(t => t.PurchaseDate).IsRequired();
+            builder.Property(t => t.IsUsed).IsRequired();
+
+            builder.Property(t => t.EventId).IsRequired();
+
+            builder.HasOne(t => t.User)
+                .WithMany(u => u.Tickets)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
