05e8f03ea155b84662b1f393a882276744409fd8
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 17:45:21 +0300

    UserConfiguration added

---
+++ NexusTix.Persistence/Repositories/Users/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Users
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
+
+            builder.HasMany(u => u.Tickets)
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
