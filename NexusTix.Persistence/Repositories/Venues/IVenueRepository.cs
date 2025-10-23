2189d6702d05d68d1cdecdf3c28cd4c17925723b
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 17:04:24 +0300

    IVenueRepository and VenueRepository added

---
+++ NexusTix.Persistence/Repositories/Venues/IVenueRepository.cs
@@ -0,0 +1,12 @@
+﻿using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Venues
+{
+    public interface IVenueRepository : IGenericRepository<Venue, int>
+    {
+        Task<IEnumerable<Venue>> GetVenuesByCityAsync(int cityId);
+        Task<IEnumerable<Venue>> GetVenuesByDistrictAsync(int districtId);
+        Task<Venue?> GetVenueWithEventsAsync(int id);
+        Task<IEnumerable<Venue>> GetVenuesWithEventsAsync();
+    }
+}
---
+++ NexusTix.Persistence/Repositories/Venues/VenueRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.Venues
+{
+    public class VenueRepository : GenericRepository<Venue, int>, IVenueRepository
+    {
+        private readonly AppDbContext _context;
+
+        public VenueRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Venue>> GetVenuesByCityAsync(int cityId)
+        {
+            return await _context.Venues.Include(x => x.District).Where(x => x.District.CityId == cityId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Venue>> GetVenuesByDistrictAsync(int districtId)
+        {
+            return await _context.Venues.Include(x => x.District).ThenInclude(x => x.City).Where(x => x.DistrictId == districtId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Venue>> GetVenuesWithEventsAsync()
+        {
+            return await _context.Venues.Include(x => x.Events).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Venue?> GetVenueWithEventsAsync(int id)
+        {
+            return await _context.Venues.Include(x => x.Events).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
