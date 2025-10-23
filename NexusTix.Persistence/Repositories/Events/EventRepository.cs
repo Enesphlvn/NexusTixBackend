6df08486dfe71cbefb08c37b03893637ceb89b86
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-23 15:35:51 +0300

    IEventRepository and EventRepository added

---
+++ NexusTix.Persistence/Repositories/Events/EventRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Domain.Entities;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.Events
+{
+    public class EventRepository : GenericRepository<Event, int>, IEventRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EventRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            return await _context.Events.Where(x => x.Date >= startDate && x.Date <= endDate).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsByEventTypeAsync(int eventTypeId)
+        {
+            return await _context.Events.Include(x => x.EventType).Where(x => x.EventTypeId == eventTypeId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsByPriceRangeAsync(decimal minPrice, decimal maxPrice)
+        {
+            return await _context.Events.Where(x => x.Price >= minPrice && x.Price <= maxPrice).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsByVenueAsync(int venueId)
+        {
+            return await _context.Events.Include(x => x.Venue).Where(x => x.VenueId == venueId).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsWithDetailAsync()
+        {
+            return await _context.Events.Include(x => x.EventType).Include(x => x.Venue).ThenInclude(x => x.District)
+                .ThenInclude(x => x.City).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsWithHighestSalesAsync(int numberOfEvents)
+        {
+            return await _context.Events.AsNoTracking().OrderByDescending(x => x.Tickets.Count())
+                .Take(numberOfEvents).ToListAsync();
+        }
+
+        public async Task<Event?> GetEventWithDetailAsync(int eventId)
+        {
+            return await _context.Events.Include(x => x.EventType).Include(x => x.Venue).ThenInclude(x => x.District)
+                .ThenInclude(x => x.City).AsNoTracking().FirstOrDefaultAsync(x => x.Id == eventId);
+        }
+    }
+}
---
+++ NexusTix.Persistence/Repositories/Events/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using NexusTix.Domain.Entities;
+
+namespace NexusTix.Persistence.Repositories.Events
+{
+    public interface IEventRepository : IGenericRepository<Event, int>
+    {
+        Task<Event?> GetEventWithDetailAsync(int eventId);
+        Task<IEnumerable<Event>> GetEventsWithDetailAsync();
+        Task<IEnumerable<Event>> GetEventsByEventTypeAsync(int eventTypeId);
+        Task<IEnumerable<Event>> GetEventsByVenueAsync(int venueId);
+        Task<IEnumerable<Event>> GetEventsWithHighestSalesAsync(int numberOfEvents);
+        Task<IEnumerable<Event>> GetEventsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate);
+        Task<IEnumerable<Event>> GetEventsByPriceRangeAsync(decimal minPrice, decimal maxPrice);
+    }
+}
