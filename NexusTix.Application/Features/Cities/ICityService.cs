be538ae88723685b2ac921e8757017149cce6a5d
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-24 15:49:52 +0300

    added ICityService interface

---
+++ NexusTix.Application/Features/Cities/ICityService.cs
@@ -0,0 +1,23 @@
+﻿using NexusTix.Application.Features.Cities.Create;
+using NexusTix.Application.Features.Cities.Dto;
+using NexusTix.Application.Features.Cities.Update;
+
+namespace NexusTix.Application.Features.Cities
+{
+    public interface ICityService
+    {
+        Task<ServiceResult<IEnumerable<CityResponse>>> GetAllCitiesAsync();
+        Task<ServiceResult<IEnumerable<CityResponse>>> GetPagedAllCitiesAsync(int pageNumber, int pageSize);
+        Task<ServiceResult<CityResponse>> GetByIdAsync(int id);
+        Task<ServiceResult<CityWithDistrictsResponse>> GetCityWithDistrictsAsync(int id);
+        Task<ServiceResult<IEnumerable<CityWithDistrictsResponse>>> GetCitiesWithDistrictsAsync();
+        Task<ServiceResult<CityWithVenuesResponse>> GetCityWithVenuesAsync(int id);
+        Task<ServiceResult<IEnumerable<CityWithVenuesResponse>>> GetCitiesWithVenuesAsync();
+        Task<ServiceResult<CityWithDistrictsAndVenuesResponse>> GetCityWithDistrictsAndVenuesAsync(int id);
+        Task<ServiceResult<IEnumerable<CityWithDistrictsAndVenuesResponse>>> GetCitiesWithDistrictsAndVenuesAsync();
+        Task<ServiceResult<CityResponse>> CreateAsync(CreateCityRequest request);
+        Task<ServiceResult> UpdateAsync(UpdateCityRequest request);
+        Task<ServiceResult> DeleteAsync(int id);
+        Task<ServiceResult> PassiveAsync(int id);
+    }
+}
