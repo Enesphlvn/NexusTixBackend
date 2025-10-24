3edd2418dece2abca3627601db5ea30c4bb1b291
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-24 15:23:34 +0300

    CityProfileMapping added

---
+++ NexusTix.Application/Features/Cities/CityProfileMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NexusTix.Application.Features.Cities.Create;
+using NexusTix.Application.Features.Cities.Dto;
+using NexusTix.Application.Features.Cities.Update;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Application.Features.Cities
+{
+    public class CityProfileMapping : Profile
+    {
+        public CityProfileMapping()
+        {
+            CreateMap<CreateCityRequest, City>();
+            CreateMap<UpdateCityRequest, City>();
+            CreateMap<City, CityResponse>();
+            CreateMap<City, CityWithDistrictsResponse>();
+            CreateMap<City, CityWithVenuesResponse>();
+            CreateMap<City, CityWithDistrictsAndVenuesResponse>();
+        }
+    }
+}
