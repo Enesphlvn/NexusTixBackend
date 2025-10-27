d01c523cf72d52e2e9678259d132042e30def7d1
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-27 15:29:17 +0300

    Added special business rules for Venue

---
+++ NexusTix.Application/Features/Venues/Rules/IVenueBusinessRules.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusTix.Application.Features.Venues.Rules
+{
+    public interface IVenueBusinessRules
+    {
+        Task CheckIfDistrictExists(int districtId);
+        Task CheckIfVenueExists(int venueId);
+        Task CheckIfVenueNameExistsWhenCreating(string venueName);
+        Task CheckIfVenueNameExistsWhenUpdating(int venueId, string venueName);
+    }
+}
---
+++ NexusTix.Application/Features/Venues/Rules/VenueBusinessRules.cs
@@ -0,0 +1,51 @@
+﻿using NexusTix.Domain.Exceptions;
+using NexusTix.Persistence.Repositories;
+
+namespace NexusTix.Application.Features.Venues.Rules
+{
+    public class VenueBusinessRules : IVenueBusinessRules
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VenueBusinessRules(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CheckIfDistrictExists(int districtId)
+        {
+            var exists = await _unitOfWork.Districts.AnyAsync(districtId);
+            if (!exists)
+            {
+                throw new BusinessException($"ID'si {districtId} olan ilçe bulunamadı.");
+            }
+        }
+
+        public async Task CheckIfVenueExists(int venueId)
+        {
+            var exists = await _unitOfWork.Venues.AnyAsync(venueId);
+            if (!exists)
+            {
+                throw new BusinessException($"ID'si {venueId} olan mekan bulunamadı.");
+            }
+        }
+
+        public async Task CheckIfVenueNameExistsWhenCreating(string venueName)
+        {
+            var exists = await _unitOfWork.Venues.AnyAsync(x => x.Name.ToLower() == venueName.ToLower());
+            if (exists)
+            {
+                throw new BusinessException($"Mekan adı: {venueName}. Bu isimde başka bir mekan mevcut");
+            }
+        }
+
+        public async Task CheckIfVenueNameExistsWhenUpdating(int venueId, string venueName)
+        {
+            var exists = await _unitOfWork.Venues.AnyAsync(x => x.Name.ToLower() == venueName.ToLower() && x.Id != venueId);
+            if (exists)
+            {
+                throw new BusinessException("Aynı isimde başka bir mekan mevcut.");
+            }
+        }
+    }
+}
