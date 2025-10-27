8f84df1732419ff29c60b9866f5c8aefc28d9ab5
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-27 15:20:08 +0300

    added BusinessException class to Domain layer

---
+++ NexusTix.Domain/Exceptions/BusinessException.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusTix.Domain.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message) { }
+        public BusinessException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
