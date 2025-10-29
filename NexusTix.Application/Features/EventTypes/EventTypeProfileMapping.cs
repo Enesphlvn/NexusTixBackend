5adbefde3e870a34f6a637f694d9a3b93b918b7d
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-29 14:17:36 +0300

    added EventTypeProfileMapping class

---
+++ NexusTix.Application/Features/EventTypes/EventTypeProfileMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NexusTix.Application.Features.EventTypes.Create;
+using NexusTix.Application.Features.EventTypes.Responses;
+using NexusTix.Application.Features.EventTypes.Update;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Application.Features.EventTypes
+{
+    public class EventTypeProfileMapping : Profile
+    {
+        public EventTypeProfileMapping()
+        {
+            CreateMap<CreateEventTypeRequest, EventType>();
+            CreateMap<UpdateEventTypeRequest, EventType>();
+
+            CreateMap<EventType, EventTypeResponse>();
+            CreateMap<EventType, EventTypeAggregateResponse>();
+            CreateMap<EventType, EventTypeWithEventsResponse>();
+        }
+    }
+}
