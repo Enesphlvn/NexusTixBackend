70b73687b4530f95b3e21b89d608ec9f54606e1a
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-29 18:13:44 +0300

    Added UserProfileMapping class

--- NexusTix.Application/Features/Users/Responses/GetUserWithTicketsResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿using NexusTix.Application.Features.Tickets.Responses;
-
-namespace NexusTix.Application.Features.Users.Responses;
-
-public record GetUserWithTicketsResponse(int Id, string FirstName, string LastName, string Email, string? PhoneNumber, IEnumerable<TicketResponse> Tickets, bool IsActive, DateTimeOffset Created, DateTimeOffset? Updated);
---
+++ NexusTix.Application/Features/Users/UserProfileMapping.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using NexusTix.Application.Features.Users.Create;
+using NexusTix.Application.Features.Users.Responses;
+using NexusTix.Application.Features.Users.Update;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Application.Features.Users
+{
+    public class UserProfileMapping : Profile
+    {
+        public UserProfileMapping()
+        {
+            CreateMap<CreateUserRequest, User>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.ToLower()));
+
+            CreateMap<UpdateUserRequest, User>()
+                .ForMember(dest => dest.Email, opt => opt.Ignore())
+                .ForMember(dest => dest.UserName, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+
+            CreateMap<User, UserResponse>();
+            CreateMap<User, UserWithTicketsResponse>();
+            CreateMap<User, UserAggregateResponse>();
+        }
+    }
+}
