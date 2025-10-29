83b156b3eb50cebda551893c9d84ef379eecdc09
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-29 16:12:35 +0300

    Added aggregate methods for User

--- NexusTix.Persistence/Repositories/Users/IUserRepository.cs
+++ NexusTix.Persistence/Repositories/Users/IUserRepository.cs
@@ -7,5 +7,7 @@ namespace NexusTix.Persistence.Repositories.Users
         Task<User?> GetUserWithTicketsAsync(int id);
         Task<IEnumerable<User>> GetUsersWithTicketsAsync();
         Task<User> GetuserbyEmailAsync(string email);
+        Task<User?> GetUserAggregateAsync(int id);
+        Task<IEnumerable<User>> GetUsersAggregateAsync();
     }
 }
--- NexusTix.Persistence/Repositories/Users/UserRepository.cs
+++ NexusTix.Persistence/Repositories/Users/UserRepository.cs
@@ -13,9 +13,25 @@ namespace NexusTix.Persistence.Repositories.Users
             _context = context;
         }
 
+        public async Task<User?> GetUserAggregateAsync(int id)
+        {
+            return await _context.Users
+                .Include(x => x.Tickets).ThenInclude(x => x.Event).ThenInclude(x => x.EventType)
+                .Include(x => x.Tickets).ThenInclude(x => x.Event).ThenInclude(x => x.Venue).ThenInclude(x => x.District).ThenInclude(x => x.City)
+                .AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
         public async Task<User> GetuserbyEmailAsync(string email)
         {
             return (await _context.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Email == email))!;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAggregateAsync()
+        {
+            return await _context.Users
+                .Include(x => x.Tickets).ThenInclude(x => x.Event).ThenInclude(x => x.EventType)
+                .Include(x => x.Tickets).ThenInclude(x => x.Event).ThenInclude(x => x.Venue).ThenInclude(x => x.District).ThenInclude(x => x.City)
+                .AsNoTracking().ToListAsync();
         }
 
         public async Task<IEnumerable<User>> GetUsersWithTicketsAsync()
