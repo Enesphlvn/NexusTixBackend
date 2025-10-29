af4e1d9005a21f8ee7f9edded5bfa8ce62ad9db3
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-29 14:00:51 +0300

    added validators for eventType requests

---
+++ NexusTix.Application/Features/EventTypes/Create/CreateEventTypeRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace NexusTix.Application.Features.EventTypes.Create
+{
+    public class CreateEventTypeRequestValidator : AbstractValidator<CreateEventTypeRequest>
+    {
+        public CreateEventTypeRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Etkinlik türü adı boş bırakılamaz.")
+                .MaximumLength(50).WithMessage("Etkinlik türü adı 50 karakterden uzun olamaz.")
+                .MinimumLength(3).WithMessage("Etkinlik türü adı 3 karakterden kısa olamaz.");
+        }
+    }
+}
---
+++ NexusTix.Application/Features/EventTypes/Update/UpdateEventTypeRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace NexusTix.Application.Features.EventTypes.Update
+{
+    public class UpdateEventTypeRequestValidator : AbstractValidator<UpdateEventTypeRequest>
+    {
+        public UpdateEventTypeRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Etkinlik türü Id'si sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Etkinlik türü adı boş bırakılamaz.")
+                .MaximumLength(50).WithMessage("Etkinlik türü adı 50 karakterden uzun olamaz.")
+                .MinimumLength(3).WithMessage("Etkinlik türü adı 3 karakterden kısa olamaz.");
+        }
+    }
+}
