ab08c377fc8ca28e2973cac33973355be594bd25
Enesphlvn <pehlivan.enes53@gmail.com>
2025-10-29 14:29:50 +0300

    added business rules for eventType

---
+++ NexusTix.Application/Features/EventTypes/Rules/EventTypeBusinessRules.cs
@@ -0,0 +1,60 @@
+﻿using NexusTix.Domain.Exceptions;
+using NexusTix.Persistence.Repositories;
+using System.Net;
+
+namespace NexusTix.Application.Features.EventTypes.Rules
+{
+    public class EventTypeBusinessRules : IEventTypeBusinessRules
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EventTypeBusinessRules(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CheckIfEventTypeExists(int eventTypeId)
+        {
+            var exists = await _unitOfWork.EventTypes.AnyAsync(x => x.Id == eventTypeId);
+            if (!exists)
+            {
+                throw new BusinessException($"ID'si {eventTypeId} olan etkinlik türü bulunamadı.", HttpStatusCode.NotFound);
+            }
+        }
+
+        public async Task CheckIfEventTypeHasNoEvents(int eventTypeId)
+        {
+            var hasEvents = await _unitOfWork.Events.AnyAsync(x => x.EventTypeId == eventTypeId);
+            if (hasEvents)
+            {
+                throw new BusinessException($"ID'si {eventTypeId} olan etkinlik türüne ait kayıtlı etkinlikler mevcuttur! Silinemez.", HttpStatusCode.Conflict);
+            }
+        }
+
+        public async Task CheckIfEventTypeNameExistsWhenCreating(string eventTypeName)
+        {
+            var exists = await _unitOfWork.EventTypes.AnyAsync(x => x.Name.ToLower() == eventTypeName.ToLower());
+            if (exists)
+            {
+                throw new BusinessException($"Etkinlik türü adı: {eventTypeName}. Bu isimde başka bir etkinlik türü mevcut", HttpStatusCode.Conflict);
+            }
+        }
+
+        public async Task CheckIfEventTypeNameExistsWhenUpdating(int eventTypeId, string eventTypeName)
+        {
+            var exists = await _unitOfWork.EventTypes.AnyAsync(x => x.Name.ToLower() == eventTypeName.ToLower() && x.Id != eventTypeId);
+            if (exists)
+            {
+                throw new BusinessException($"Etkinlik türü adı: {eventTypeName}. Bu isimde başka bir etkinlik türü mevcut", HttpStatusCode.Conflict);
+            }
+        }
+
+        public void CheckIfPagingParametersAreValid(int pageNumber, int pageSize)
+        {
+            if (pageNumber <= 0 || pageSize <= 0)
+            {
+                throw new BusinessException("Sayfa numarası veya boyutu sıfırdan büyük olmalıdır.", HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ NexusTix.Application/Features/EventTypes/Rules/IEventTypeBusinessRules.cs
@@ -0,0 +1,12 @@
+﻿using NexusTix.Application.Common.BaseRules;
+
+namespace NexusTix.Application.Features.EventTypes.Rules
+{
+    public interface IEventTypeBusinessRules : IBaseBusinessRules
+    {
+        Task CheckIfEventTypeExists(int eventTypeId);
+        Task CheckIfEventTypeNameExistsWhenCreating(string eventTypeName);
+        Task CheckIfEventTypeNameExistsWhenUpdating(int eventTypeId, string eventTypeName);
+        Task CheckIfEventTypeHasNoEvents(int eventTypeId);
+    }
+}
