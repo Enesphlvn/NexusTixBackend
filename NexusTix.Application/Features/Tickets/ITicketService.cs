86376f564dc27abf920d6f4dae43fed359b10936
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-01 17:53:15 +0300

    Added ITicketService

---
+++ NexusTix.Application/Features/Tickets/ITicketService.cs
@@ -0,0 +1,26 @@
+﻿using NexusTix.Application.Features.Tickets.CheckIn;
+using NexusTix.Application.Features.Tickets.Create;
+using NexusTix.Application.Features.Tickets.Responses;
+
+namespace NexusTix.Application.Features.Tickets
+{
+    public interface ITicketService
+    {
+        Task<ServiceResult<IEnumerable<TicketResponse>>> GetAllTicketsAsync();
+        Task<ServiceResult<IEnumerable<TicketResponse>>> GetPagedAllTicketsAsync(int pageNumber, int pageSize);
+        Task<ServiceResult<TicketResponse>> GetByIdAsync(int id);
+
+        Task<ServiceResult<TicketAggregateResponse>> GetTicketAggregateAsync(int id);
+        Task<ServiceResult<IEnumerable<TicketAggregateResponse>>> GetTicketsAggregateAsync();
+        Task<ServiceResult<IEnumerable<TicketByEventResponse>>> GetTicketsByEventAsync(int eventId);
+        Task<ServiceResult<IEnumerable<TicketByUserResponse>>> GetTicketsByUserAsync(int userId);
+        Task<ServiceResult<IEnumerable<TicketByDateRangeResponse>>> GetTicketsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate);
+        Task<ServiceResult<bool>> HasUserTicketForEventAsync(int userId, int eventId);
+        Task<ServiceResult<int>> GetTicketCountByEventAsync(int eventId);
+
+        Task<ServiceResult<TicketResponse>> CreateAsync(CreateTicketRequest request);
+        Task<ServiceResult> CheckInAsync(CheckInTicketRequest request);
+        Task<ServiceResult> DeleteAsync(int id);
+        Task<ServiceResult> PassiveAsync(int id);
+    }
+}
---
+++ NexusTix.Application/Features/Tickets/TicketService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using NexusTix.Application.Features.Tickets.CheckIn;
+using NexusTix.Application.Features.Tickets.Create;
+using NexusTix.Application.Features.Tickets.Responses;
+using NexusTix.Application.Features.Tickets.Rules;
+using NexusTix.Persistence.Repositories;
+
+namespace NexusTix.Application.Features.Tickets
+{
+    public class TicketService : ITicketService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ITicketBusinessRules _ticketRules;
+
+        public TicketService(IUnitOfWork unitOfWork, IMapper mapper, ITicketBusinessRules ticketRules)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _ticketRules = ticketRules;
+        }
+
+        public Task<ServiceResult> CheckInAsync(CheckInTicketRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<TicketResponse>> CreateAsync(CreateTicketRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketResponse>>> GetAllTicketsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<TicketResponse>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketResponse>>> GetPagedAllTicketsAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<TicketAggregateResponse>> GetTicketAggregateAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<int>> GetTicketCountByEventAsync(int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketAggregateResponse>>> GetTicketsAggregateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketByDateRangeResponse>>> GetTicketsByDateRangeAsync(DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketByEventResponse>>> GetTicketsByEventAsync(int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<IEnumerable<TicketByUserResponse>>> GetTicketsByUserAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult<bool>> HasUserTicketForEventAsync(int userId, int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult> PassiveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
