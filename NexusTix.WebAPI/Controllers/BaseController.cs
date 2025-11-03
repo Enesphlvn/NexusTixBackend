3b69b5cafb90d060a05194dd668195c999f77a14
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-03 16:10:01 +0300

    Added BaseController

---
+++ NexusTix.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexusTix.Application;
+using System.Net;
+
+namespace NexusTix.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(ServiceResult<T> result)
+        {
+            return result.Status switch
+            {
+                HttpStatusCode.NoContent => NoContent(),
+                HttpStatusCode.Created => Created(result.UrlAsCreated, result),
+                _ => new ObjectResult(result) { StatusCode = result.Status.GetHashCode() }
+            };
+        }
+
+        [NonAction]
+        public IActionResult CreateActionResult(ServiceResult result)
+        {
+            return result.Status switch
+            {
+                HttpStatusCode.NoContent => new ObjectResult(null) { StatusCode = result.Status.GetHashCode() },
+                _ => new ObjectResult(result) { StatusCode = result.Status.GetHashCode() }
+            };
+        }
+    }
+}
