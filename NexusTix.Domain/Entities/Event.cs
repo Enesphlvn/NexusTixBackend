c0626ac5cc5f6e339f1df76f9c2f07d422892059
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-03 13:12:59 +0300

    Added capacity propert to Event entity

--- NexusTix.Domain/Entities/Event.cs
+++ NexusTix.Domain/Entities/Event.cs
@@ -8,6 +8,7 @@ namespace NexusTix.Domain.Entities
         public DateTimeOffset Date { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
+        public int Capacity { get; set; }
         public int EventTypeId { get; set; }
         public EventType EventType { get; set; } = default!;
         public int VenueId { get; set; }
