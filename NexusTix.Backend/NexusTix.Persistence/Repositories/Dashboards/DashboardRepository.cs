a63c0316f76aba4ba1aec6aeba7ba450d7d9738e
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-16 20:07:31 +0300

    Added IDashboardRepository and DashboardRepository to persistence layer

---
+++ NexusTix.Backend/NexusTix.Persistence/Repositories/Dashboards/DashboardRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusTix.Persistence.Context;
+
+namespace NexusTix.Persistence.Repositories.Dashboards
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly AppDbContext _context;
+        public DashboardRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GetActiveEventsCountAsync()
+        {
+            return await _context.Events.CountAsync(e => e.Date > DateTimeOffset.UtcNow && e.IsActive);
+        }
+
+        public async Task<decimal> GetTotalRevenueAsync()
+        {
+            return await _context.Tickets.Where(x => !x.IsCancelled).SumAsync(x => x.Event.Price);
+        }
+
+        public async Task<int> GetTotalTicketsSoldAsync()
+        {
+            return await _context.Tickets.CountAsync(t => !t.IsCancelled);
+        }
+
+        public async Task<int> GetTotalUsersCountAsync()
+        {
+            return await _context.Users.CountAsync(u => u.IsActive);
+        }
+    }
+}
---
+++ NexusTix.Backend/NexusTix.Persistence/Repositories/Dashboards/IDashboardRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusTix.Persistence.Repositories.Dashboards
+{
+    public interface IDashboardRepository
+    {
+        Task<decimal> GetTotalRevenueAsync();
+        Task<int> GetTotalTicketsSoldAsync();
+        Task<int> GetActiveEventsCountAsync();
+        Task<int> GetTotalUsersCountAsync();
+    }
+}
