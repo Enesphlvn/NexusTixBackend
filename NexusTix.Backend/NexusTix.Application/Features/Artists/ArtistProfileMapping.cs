5bc4be2e0d961ea89aa7af806513f7981fa950e8
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-27 18:20:53 +0300

    Added ArtistProfileMapping class

---
+++ NexusTix.Backend/NexusTix.Application/Features/Artists/ArtistProfileMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NexusTix.Application.Features.Artists.Create;
+using NexusTix.Application.Features.Artists.Responses;
+using NexusTix.Application.Features.Artists.Update;
+using NexusTix.Domain.Entities;
+
+namespace NexusTix.Application.Features.Artists
+{
+    public class ArtistProfileMapping : Profile
+    {
+        public ArtistProfileMapping()
+        {
+            CreateMap<CreateArtistRequest, Artist>();
+            CreateMap<UpdateArtistRequest, Artist>();
+
+            CreateMap<Artist, ArtistResponse>();
+            CreateMap<Artist, ArtistWithEventsResponse>();
+        }
+    }
+}
