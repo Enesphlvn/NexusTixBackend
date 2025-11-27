115d15f25027a633bcd6648aabf4b3001d3bc4f8
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-27 14:39:29 +0300

    Updated CreateVenueRequest and UpdateVenueRequest validators

--- NexusTix.Backend/NexusTix.Application/Features/Venues/Update/UpdateVenueRequestValidator.cs
+++ NexusTix.Backend/NexusTix.Application/Features/Venues/Update/UpdateVenueRequestValidator.cs
@@ -18,6 +18,14 @@ namespace NexusTix.Application.Features.Venues.Update
 
             RuleFor(x => x.DistrictId)
                 .GreaterThan(0).WithMessage("District Id sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.Latitude)
+                .InclusiveBetween(-90, 90)
+                .WithMessage("Enlem değeri -90 ile +90 arasında olmalıdır.");
+
+            RuleFor(x => x.Longitude)
+                .InclusiveBetween(-180, 180)
+                .WithMessage("Boylam değeri -180 ile +180 arasında olmalıdır.");
         }
     }
 }
