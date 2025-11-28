2509f38535404972505ef305383e740b9c29ceb2
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-29 00:51:12 +0300

    Refactored EventResponse

--- NexusTix.Backend/NexusTix.Application/Features/Events/Responses/EventResponse.cs
+++ NexusTix.Backend/NexusTix.Application/Features/Events/Responses/EventResponse.cs
@@ -1,3 +1,14 @@
 ﻿namespace NexusTix.Application.Features.Events.Responses;
 
-public record EventResponse(int Id, string Name, DateTimeOffset Date, decimal Price, string? Description, int Capacity, int EventTypeId, int VenueId, IEnumerable<int> ArtistIds);
+public record EventResponse
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public DateTimeOffset Date { get; init; }
+    public decimal Price { get; init; }
+    public string? Description { get; init; }
+    public int Capacity { get; init; }
+    public int EventTypeId { get; init; }
+    public int VenueId { get; init; }
+    public IEnumerable<int> ArtistIds { get; set; } = [];
+}
