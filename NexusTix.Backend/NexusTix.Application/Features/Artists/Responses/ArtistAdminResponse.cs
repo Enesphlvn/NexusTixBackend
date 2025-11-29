48f9a7e24759d7017a0469cec13b9e1d96f0c67c
Enesphlvn <pehlivan.enes53@gmail.com>
2025-11-29 15:56:44 +0300

    Created ArtistAdminResponse

---
+++ NexusTix.Backend/NexusTix.Application/Features/Artists/Responses/ArtistAdminResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace NexusTix.Application.Features.Artists.Responses
+{
+    public class ArtistAdminResponse
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string? Bio { get; init; }
+        public string? ImageUrl { get; init; }
+        public bool IsActive { get; init; }
+        public DateTimeOffset Created { get; init; }
+        public DateTimeOffset? Updated { get; init; }
+    }
+}
